24ef235c13856511cdd0f47731a5e85799d5bbfb
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-01 13:00:04 -0600

    practica4

---
+++ title_upper_lower..py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+nombre = "alexis eduardo aguilar" #se declara un string en minusculas 
+print(nombre.title())   #imprimimos el string con el formato de titulo 
+
+oracion = "Esta Es una Frase Para Ser Formateada" #declaramos string con mayusculas y minusculas 
+
+print(oracion.upper()) #imprimimos string en Mayusculas 
+print(oracion.lower()) #imprimimos string en minusculas 
