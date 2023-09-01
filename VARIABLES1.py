3f0bd6b4a63d953bcb4cc0fe320caa53f9e89a5e
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-01 01:06:04 -0600

    practica1

---
+++ VARIABLES1.py
@@ -0,0 +1,8 @@
+
+saludo = "Hola mundo" #almacenamos el string en la variable 
+print(saludo) #imprimimos la variable en el valor contenido 
+
+x=5 #Se declara la variable x
+y=10 #Se declara la variable y
+z=x+y #Sumamos las variables previamente establecidas y se almacenaran en la varible z
+print(z) #Se imprime lo almacenado en z
