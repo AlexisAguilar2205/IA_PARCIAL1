2fac2b4628ade37632230219e40128468828adfb
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-01 02:51:35 -0600

    practica3

---
+++ concatenar.py
@@ -0,0 +1,28 @@
+
+###############-AREA DE PRUEBA-###################
+
+
+#PrimeraPalabra = 'progra'
+#SgundaPalabra = "facil"
+#nombre = PrimeraPalabra + " " + SgundaPalabra + "."
+#print(nombre)
+
+
+###############-AREA DE PRUEBA-###################
+PrimeraPalabra = 'progra'#asigno variable1
+
+SegundaPalabra = "facil"#asigno variable2
+
+nombre = PrimeraPalabra + " " + SegundaPalabra + "."#concateno variable 1 y variable2
+
+print(nombre)#imprimo la concatenacion de ambas variables
+nombre2 = "hola"+"que tal"#concateno strings directamente en una variable
+print(nombre2)#imprimo dicha variable
+print("string1"+" "+"string2")#concateno directamente desde la funcion print 2 strings
+
+nom1 = "Alexis " #declaro nombre1
+nom2 = "Eduardo " #declaro nombre2
+ape1 = "Aguilar " #declaro mi primer apellido
+ape2 = "Villaseñor " #declaro mi segundo apellido
+nomc = nom1+nom2+ape1+ape2 #concateno mi nombre completo
+print(nomc) #e imprimo
