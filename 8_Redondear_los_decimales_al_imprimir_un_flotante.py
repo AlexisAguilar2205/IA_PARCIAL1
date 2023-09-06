2f9c69d233252138a083cbf22e28dcd87edcb794
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-05 23:18:03 -0600

    Practica8

---
+++ 8_Redondear_los_decimales_al_imprimir_un_flotante.py
@@ -0,0 +1,8 @@
+
+#Alexis Eduardo Aguilar Villaseñor #21110343
+
+#Redondeo con decimales 
+
+valor = 1.5678910
+
+print ( round (valor, 5)) #El round se utilizara para indicar cuantos decimales usaremos y mostrremos en le print 
