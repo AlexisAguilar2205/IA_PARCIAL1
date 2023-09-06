db87bb10681020e8a8b6863c92f00f583d7eb939
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-05 23:43:00 -0600

    practica9

---
+++ _9_Creación_de_una_lista.py
@@ -0,0 +1,11 @@
+
+#Crea una lista que contenga los valores en las posiciones indicadas.
+# en la posición 4.
+# en la posición 1.
+# en la posición 0.
+# en la posición 3.
+# en la posición 2.
+
+lista  = [ "corvette" , "subaru", "nissan" , "toyota" , "ford" ] #creamos lista con las posiciones asignadas
+print ( lista ) #imprimimos toda la lista
+print ( lista [ 0 ]) #imprimimos el primer dato de la lista
