8aa1bc6b16adabaef62afe27a018000a48d4c001
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-06 00:11:49 -0600

    practica10

---
+++ _10_Posiciones_Negativas_en_listas.py
@@ -0,0 +1,4 @@
+
+#listas[-1], para llamar al ultimo elemento de la lista, -2 para llamar al penultimo 
+color = ['rojo','azul','verde','amarillo','marron','morado','negro','rosa','blanco','naranja']
+print(color[-1],color[-7],color[-5],color[-2],color[-10])
