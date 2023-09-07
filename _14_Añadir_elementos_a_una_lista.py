43c9bb1095f8052fb899a64341a39a03bc72050e
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-06 21:34:10 -0600

    practica14

---
+++ _14_Añadir_elementos_a_una_lista.py
@@ -0,0 +1,9 @@
+#Añade colores 'morado' y 'cafe'
+
+color = ['rojo','azu','verde','amarillo','marron','lila','negro','rosa','blanco','naranja']
+#Se crea la lista 
+
+color.insert(5,"morado") #Añadimos morado en la posicion6
+color.insert(10,"cafe") #Añadimos cafe en la posicion 10
+
+print(color) #imprimimos la lista 
