0f6a794a7fbf94450c355fcb9984fa66436abea0
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-06 23:54:50 -0600

    practica20

---
+++ _20_Condicional_if.py
@@ -0,0 +1,32 @@
+
+#Cambia el operador para que la condición sea True.
+    #num1 = 30
+    #num2 = 25
+    #if num1 == num2:
+    #	print('Se ejecuta el if 1.')
+num1 = 30
+num2 = 25
+if num1 != num2:
+    print('Se ejecuta el if 1.')
+
+
+#Cambia el operador para que la condición sea True.
+#num1 = 1500
+#num2 = 60
+#if num1 < num2:
+#	print('Se ejecuta el if 2.')
+num1 = 1500
+num2 = 60
+if num1 > num2:
+	print('Se ejecuta el if 2.')
+
+
+# Haz que el siguiente condicional se convierta en False sin cambiar el operador.
+#num1 = 80
+#num2 = 80
+#if num1 != num2:
+#	print('Se ejecuta el if 3.')
+num1 = 80
+num2 = 80
+if num1 != num2:
+	print('Se ejecuta el if 3.')
