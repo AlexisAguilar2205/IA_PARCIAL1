148f6cc9153bbe66526cdb8781b7b5eae55210ce
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-06 23:35:44 -0600

    practica18

---
+++ _18_Tuplas.py
@@ -0,0 +1,15 @@
+
+#Imprime la segunda posición de la primer tupla.
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+#se crea tupla llaada colores
+
+print(colores[5]) #imprimiendo con el nombre de la tupla y la posicion del elemento
+
+#Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos 
+#de la siguiente tupla en una variable llamada operacion.
+
+numeros = (20, 15, 1, 9) #nueva dupla
+
+print("la suma es: ", numeros[0]+numeros[2]+numeros[3]-numeros[1]) 
+#imprimimos y sumamos/restamos dentro del print los valores de la dupla
