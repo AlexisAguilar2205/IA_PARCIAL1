cd7de6eebf0bbb39f75ec2f4c2b90ccf5b153870
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-09-16 14:46:45 -0600

    practica31

---
+++ _31_Creacion_y_uso_de_funciones.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+#Crea una función que realice una suma. Para ello, tendrás que añadir dos argumentos (numero1 y numero2). 
+#En su interior, especificarás un print() que muestre el resultado de la suma. Deberás hacer tres llamadas 
+#que como resultado de la suma den los valores 30, 50 y 57000. Los números que introduzcas en la llamada pueden 
+#ser los que quieras siempre que coincidan los resultados en el print().
+
+def suma(num1,num2):
+    print("El resultado de la suma es: ",num1 + num2)
+
+suma(20,10)
+suma(20,30)
+suma(50000,7000)
