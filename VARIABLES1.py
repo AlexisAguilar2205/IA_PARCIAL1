b3b33e1d3a8c105425faebb9522412870dc7851e
AlexisAguilar2205 <143780084+AlexisAguilar2205@users.noreply.github.com>
2023-10-26 19:55:37 -0600

    1

--- VARIABLES1.py
+++
@@ -1,8 +0,0 @@
-
-saludo = "Hola mundo" #almacenamos el string en la variable 
-print(saludo) #imprimimos la variable en el valor contenido 
-
-x=5 #Se declara la variable x
-y=10 #Se declara la variable y
-z=x+y #Sumamos las variables previamente establecidas y se almacenaran en la varible z
-print(z) #Se imprime lo almacenado en z
--- _17_Determinar_los_elementos__len_.py
+++
@@ -1,9 +0,0 @@
-
-#contar los elementos de una lista
-
-colores = ['nissan', 'ford', 'toyota', 'bmw'] #creamos lista
-
-print(len(colores)) #imprimimos en consola el numero de elementos de la lista sin almacenar el valor
-
-longitud_colores = len(colores) #almacenamos el numero de elementos en una variable
-print(longitud_colores) #mostramos el valor
